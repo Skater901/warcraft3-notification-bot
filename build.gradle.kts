a18243d926fb057dcd85f03160efd06ed38d2010
Skater901 <skaterx901@hotmail.com>
2024-08-09 18:47:30 +1000

    Initial commit of bot, rough project structure worked out.

---
+++ build.gradle.kts
@@ -0,0 +1,37 @@
+val discord_version: String by project
+val ktx_version: String by project
+val coroutines_version: String by project
+val mysql_version: String by project
+val jdbi_version: String by project
+
+plugins {
+    kotlin("jvm") version "2.0.0"
+}
+
+group = "au.com.skater901"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    // Discord API
+    implementation("net.dv8tion:JDA:$discord_version")
+    implementation("club.minnced:jda-ktx:$ktx_version")
+
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:$coroutines_version")
+
+    // Database libraries
+    implementation("com.mysql:mysql-connector-j:$mysql_version")
+    implementation("org.jdbi:jdbi3-core:$jdbi_version")
+
+    testImplementation(kotlin("test"))
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+kotlin {
+    jvmToolchain(21)
+}

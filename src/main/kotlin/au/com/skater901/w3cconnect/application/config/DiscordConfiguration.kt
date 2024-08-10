64d63ff3308ac31f91d83210bc212088be676700
Skater901 <skaterx901@hotmail.com>
2024-08-10 17:32:12 +1000

    Remove Discord public key from config as it doesn't seem to be needed.

--- src/main/kotlin/au/com/skater901/w3cconnect/application/config/DiscordConfiguration.kt
+++ src/main/kotlin/au/com/skater901/w3cconnect/application/config/DiscordConfiguration.kt
@@ -3,12 +3,10 @@ package au.com.skater901.w3cconnect.application.config
 import java.util.*
 
 class DiscordConfiguration(
-    val publicKey: String,
     val privateToken: String
 ) {
     companion object {
         fun parse(prefix: String, properties: Properties): DiscordConfiguration = DiscordConfiguration(
-            properties.getPropertyOrThrow(prefix, DiscordConfiguration::publicKey),
             properties.getPropertyOrThrow(prefix, DiscordConfiguration::privateToken)
         )
     }
